9d0f0cf7249f74b8c8c29b8fdcc0a8e8cf8ba685
Miranda Pattyn <miranda.pattyn@gmail.com>
2018-02-09 16:26:07 -0800

    Added map

--- app/src/main/java/com/example/gordiartur/adoptablock/LandingActivity.java
+++ app/src/main/java/com/example/gordiartur/adoptablock/LandingActivity.java
@@ -1,5 +1,6 @@
 package com.example.gordiartur.adoptablock;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -24,7 +25,8 @@ public class LandingActivity extends AppCompatActivity {
                     mTextMessage.setText(R.string.title_profile);
                     return true;
                 case R.id.navigation_map:
-                    mTextMessage.setText(R.string.title_map);
+                    Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                    startActivity(intent);
                     return true;
                 case R.id.navigation_more:
                     mTextMessage.setText(R.string.title_more);
