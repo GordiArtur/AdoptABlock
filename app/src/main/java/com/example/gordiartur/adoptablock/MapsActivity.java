e1376717e15f0c981469771aab7fe04eea513aa7
Miranda Pattyn <miranda.pattyn@gmail.com>
2018-02-16 20:06:24 -0800

    Added blocks to map

--- app/src/main/java/com/example/gordiartur/adoptablock/MapsActivity.java
+++ app/src/main/java/com/example/gordiartur/adoptablock/MapsActivity.java
@@ -16,19 +16,22 @@ import com.google.android.gms.maps.model.PatternItem;
 import com.google.android.gms.maps.model.Polygon;
 import com.google.android.gms.maps.model.PolygonOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class MapsActivity extends FragmentActivity
         implements OnMapReadyCallback,
         GoogleMap.OnPolygonClickListener {
 
-    private static final int COLOR_BLACK_ARGB = 0xff000000;
-    private static final int COLOR_WHITE_ARGB = 0xffffffff;
     private static final int COLOR_GREEN_ARGB = 0xff388E3C;
     private static final int COLOR_PURPLE_ARGB = 0xff81C784;
-    private static final int COLOR_ORANGE_ARGB = 0xffF57F17;
-    private static final int COLOR_BLUE_ARGB = 0xffF9A825;
 
     private static final int POLYGON_STROKE_WIDTH_PX = 8;
     private static final int PATTERN_DASH_LENGTH_PX = 20;
@@ -37,11 +40,8 @@ public class MapsActivity extends FragmentActivity
     private static final PatternItem DASH = new Dash(PATTERN_DASH_LENGTH_PX);
     private static final PatternItem GAP = new Gap(PATTERN_GAP_LENGTH_PX);
 
-    // Create a stroke pattern of a gap followed by a dash.
-    private static final List<PatternItem> PATTERN_POLYGON_ALPHA = Arrays.asList(GAP, DASH);
-
     // Create a stroke pattern of a dot followed by a gap, a dash, and another gap.
-    private static final List<PatternItem> PATTERN_POLYGON_BETA =
+    private static final List<PatternItem> PATTERN_POLYGON_ALPHA =
             Arrays.asList(DOT, GAP, DASH, GAP);
 
     @Override
@@ -54,45 +54,59 @@ public class MapsActivity extends FragmentActivity
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        // Add polygons to indicate areas on the map.
-        Polygon polygon1 = googleMap.addPolygon(new PolygonOptions()
-                .clickable(true)
-                .add(
-                        new LatLng(-27.457, 153.040),
-                        new LatLng(-33.852, 151.211),
-                        new LatLng(-37.813, 144.962),
-                        new LatLng(-34.928, 138.599)));
-        // Store a data object with the polygon, used here to indicate an arbitrary type.
-        polygon1.setTag("alpha");
-        // Style the polygon.
-        stylePolygon(polygon1);
+        InputStream inputStream = getResources().openRawResource(R.raw.blocks);
 
-        Polygon polygon2 = googleMap.addPolygon(new PolygonOptions()
-                .clickable(true)
-                .add(
-                        new LatLng(-31.673, 128.892),
-                        new LatLng(-31.952, 115.857),
-                        new LatLng(-17.785, 122.258),
-                        new LatLng(-12.4258, 130.7932)));
-        polygon2.setTag("beta");
-        stylePolygon(polygon2);
+        Scanner scan = new Scanner(inputStream);
 
-        // Position the map's camera near Alice Springs in the center of Australia,
-        // and set the zoom factor so most of Australia shows on the screen.
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-23.684, 133.903), 4));
+        String str = "";
 
-        // Set listeners for click events.
+        while(scan.hasNextLine()) {
+            String line = scan.nextLine();
+
+            str = str + line;
+        }
+
+        try {
+            JSONObject root = new JSONObject(str);
+            JSONArray features = root.getJSONArray("features");
+
+            for (int i = 0; i < features.length(); i++) {
+                JSONObject first = features.getJSONObject(i);
+                JSONObject geometry = first.getJSONObject("geometry");
+                JSONArray coordinates = geometry.getJSONArray("coordinates");
+
+                for (int j = 0; j < coordinates.length(); j++) {
+
+                    JSONArray second = coordinates.getJSONArray(j);
+                    ArrayList<LatLng> latLng = new ArrayList<LatLng>();
+
+                    for (int k = 0; k < second.length(); k++) {
+
+                        JSONArray third = second.getJSONArray(k);
+
+                        latLng.add(new LatLng(third.getDouble(1), third.getDouble(0)));
+                    }
+
+                    PolygonOptions options = new PolygonOptions();
+
+                    for (LatLng location : latLng) {
+                        options.add(location);
+                    }
+
+                    Polygon polygon = googleMap.addPolygon(options.clickable(true));
+                    polygon.setTag(j);
+                    stylePolygon(polygon);
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        LatLng newWest = new LatLng(49.2057, -122.9110);
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(newWest, 13));
         googleMap.setOnPolygonClickListener(this);
     }
 
@@ -101,36 +115,10 @@ public class MapsActivity extends FragmentActivity
      * @param polygon The polygon object that needs styling.
      */
     private void stylePolygon(Polygon polygon) {
-        String type = "";
-        // Get the data object stored with the polygon.
-        if (polygon.getTag() != null) {
-            type = polygon.getTag().toString();
-        }
-
-        List<PatternItem> pattern = null;
-        int strokeColor = COLOR_BLACK_ARGB;
-        int fillColor = COLOR_WHITE_ARGB;
-
-        switch (type) {
-            // If no type is given, allow the API to use the default.
-            case "alpha":
-                // Apply a stroke pattern to render a dashed line, and define colors.
-                pattern = PATTERN_POLYGON_ALPHA;
-                strokeColor = COLOR_GREEN_ARGB;
-                fillColor = COLOR_PURPLE_ARGB;
-                break;
-            case "beta":
-                // Apply a stroke pattern to render a line of dots and dashes, and define colors.
-                pattern = PATTERN_POLYGON_BETA;
-                strokeColor = COLOR_ORANGE_ARGB;
-                fillColor = COLOR_BLUE_ARGB;
-                break;
-        }
-
-        polygon.setStrokePattern(pattern);
+        polygon.setStrokePattern(PATTERN_POLYGON_ALPHA);
         polygon.setStrokeWidth(POLYGON_STROKE_WIDTH_PX);
-        polygon.setStrokeColor(strokeColor);
-        polygon.setFillColor(fillColor);
+        polygon.setStrokeColor(COLOR_GREEN_ARGB);
+        polygon.setFillColor(COLOR_PURPLE_ARGB);
     }
 
     /**
@@ -145,6 +133,6 @@ public class MapsActivity extends FragmentActivity
         color = polygon.getFillColor() ^ 0x00ffffff;
         polygon.setFillColor(color);
 
-        Toast.makeText(this, "Area type " + polygon.getTag().toString(), Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Area number " + polygon.getTag().toString(), Toast.LENGTH_SHORT).show();
     }
 }
